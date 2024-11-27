498055ff744d1ae3a87fed01e005fca098457257
bwondessen <bwondessen23@gmail.com>
2024-11-26 18:06:25 -0600

    Clear cart when user confirms prompt

--- script.js
+++ script.js
@@ -137,6 +137,22 @@ class ShoppingCart {
         return this.items.length;
     }
 
+    clearCart() {
+        if (!this.items.length) {
+            alert("Your shopping cart is already empty");
+            return;
+        }
+
+        const isCartCleared = confirm(
+            "Are you sure you want to clear all items from your shopping cart?"
+        );
+
+        if (isCartCleared) {
+            this.items = [];
+            this.total = 0;
+        }
+    }
+
     calculateTaxes(amount) {
         return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
     }
@@ -160,6 +176,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
         btn.addEventListener("click", (event) => {
             cart.addItem(Number(event.target.id), products);
             totalNumberOfItems.textContent = cart.getCounts();
+            cart.calculateTotal();
         })
     }
 );
